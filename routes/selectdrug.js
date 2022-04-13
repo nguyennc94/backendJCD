aaeb28ccb524c9765c5f05413824520b838bd896
Nguyen <Nnguyen52@aceproject.space>
2022-04-13 12:08:49 -0500

    Test search

--- routes/selectdrug.js
+++ routes/selectdrug.js
@@ -13,7 +13,14 @@ router.get('/name/:name', async (req, res) => {
     const database = new Prohairesis(env.CLEARDB_DATABASE_URL);
 
     database
-    .query(`SELECT * FROM yanksk_pw_drugs  WHERE LOWER(name) LIKE LOWER('${req.params.name}%')`)
+    .query(`
+      SELECT YAnkSk_pw_drugs.drug_id, YAnkSk_pw_drugs.name, YAnkSk_pw_ingredients.ingredient_id, YAnkSk_pw_ingredients.ingredient_name
+      FROM YAnkSk_pw_drugs
+      JOIN YAnkSk_pw_drug_ingredient ON YAnkSk_pw_drugs.drug_id = YAnkSk_pw_drug_ingredient.drug_id
+      JOIN YAnkSk_pw_ingredients ON YAnkSk_pw_drug_ingredient.ingredient_id = YAnkSk_pw_ingredients.ingredient_id
+      WHERE LOWER(YAnkSk_pw_drugs.name) LIKE LOWER('${req.params.name}%')
+      OR LOWER(YAnkSk_pw_ingredients.ingredient_name) LIKE LOWER('${req.params.name}%')
+    `)
     .then((response) => {
       console.log(response);
       // const jsonResponse = JSON.stringify(response);
